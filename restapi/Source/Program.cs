eafedfbc1958bc6ab48ec7a9d0283c93a646a84a
Aron Saengchan <acsaeng@gmail.com>
2025-04-09 20:27:31 -0600

    Add env variable to rest api service

--- restapi/Source/Program.cs
+++ restapi/Source/Program.cs
@@ -30,7 +30,7 @@ using (var scope = app.Services.CreateScope())
   db.Database.Migrate();
 }
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
   app.UseSwagger();
